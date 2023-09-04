60d44db794607772f5f9fd548687b06c69909f50
RosaUlisses <ulissao@proton.me>
2023-09-04 09:48:31 -0300

    add ambitious_kid solution

---
+++ Competitive-Programming/basics/ambitious_kid.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int mod(int n) {
+    return n < 0 ? n * -1 : n;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    
+    vector<int> array(n);
+    for (int i = 0; i < n; ++i) {
+        cin >> array[i]; 
+    }
+    
+    int min = mod(array[0]);
+
+    for (int i = 1; i < array.size(); ++i) {
+        int value = mod(array[i]);
+        if (value < min) {
+            min = value;
+        } 
+    }
+    
+    cout << min; 
+    
+	return 0;
+}
