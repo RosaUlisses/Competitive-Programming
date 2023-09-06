7061318f99a31588d847572d02f72993ff855665
RosaUlisses <ulissao@proton.me>
2023-09-06 11:41:31 -0300

    add solution of the problems: knapsack1 and coast

---
+++ Competitive-Programming/binary_search-sort/knapsack1.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+
+int main() {
+    int n, w;
+    cin >> n >> w;
+    
+    vector<pair<int,int64_t >> array;
+
+    for (int i = 0; i < n; ++i) {
+        int weight;
+        int64_t value;
+        cin >> weight >> value; 
+        array.push_back({weight, value});
+    }
+    
+    vector<vector<int64_t>> matrix(n + 1);
+    for (auto & i : matrix) {
+        for (int j = 0; j < w + 1; ++j) {
+            i.push_back(0);
+        }       
+    }
+    
+    for (int i = 1; i < matrix.size(); ++i) {
+        int weight = array[i - 1].first;
+        int64_t value = array[i - 1].second; 
+        for (int j = 0; j < matrix[i].size(); ++j) {
+            if (j >= weight) {
+                matrix[i][j] = value;
+            }
+            if (j - weight >= 0) {
+                matrix[i][j] += matrix[i - 1][j - weight];
+            }
+            if (i - 1 >= 0 && matrix[i - 1][j] > matrix[i][j]) {
+                matrix[i][j] = matrix[i - 1][j];
+            }
+        } 
+    }
+
+    cout << matrix[n][w]; 
+    
+	return 0;
+}
---
+++ Competitive-Programming/graph/coast.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+/*
+    Judge: Neps Academy 
+    Problem: Coast 
+    url: https://neps.academy/exercise/329
+ */
+
+bool is_adjacent_to_water(int i, int j, vector<vector<char>>& matrix) {
+    if ((i + 1 >= matrix.size() || i - 1 < 0 || j + 1 >= matrix[0].size() || j - 1 < 0)) {
+        if (matrix[i][j] == '#') return true;
+        return false;
+    }
+    if((matrix[i + 1][j] == '.' || matrix[i - 1][j] == '.' || matrix[i][j - 1] == '.' || matrix[i][j + 1] == '.') && matrix[i][j] == '#') return true; 
+    return false;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<char>> matrix;
+
+    for (int i = 0; i < n; ++i) {
+        matrix.push_back({});
+        for (int j = 0; j < m; ++j) {
+            char value; 
+            cin >> value;
+            matrix[i].push_back(value);
+        }
+    }
+
+    int number_of_coasts = 0;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (is_adjacent_to_water(i, j, matrix)) {
+                number_of_coasts++;
+            } 
+        } 
+    }
+    
+    cout << number_of_coasts;
+    
+	return 0;
+}
